1e3d548c1d2dcef017ab73f2534fa80c23172e7a
brandontzhang <zhangtbrandon2@gmail.com>
2022-09-14 22:21:07 -0400

    Added delete and update for bullets

--- server/controllers/bulletController.ts
+++ server/controllers/bulletController.ts
@@ -1,4 +1,3 @@
-import { resolveSoa } from 'dns';
 import { Request, Response, Router } from 'express';
 import db from '../db';
 import Bullet from '../models/bullets';
@@ -14,7 +13,8 @@ class BulletController {
   private initializeRoutes() {
     this.router.get(this.path, this.getBullets);
     this.router.post(this.path, this.createBullet);
-    // this.router.put(this.path, this.updateBullet);
+    this.router.put(`${this.path}/:id`, this.updateBullet);
+    this.router.delete(`${this.path}/:id`, this.deleteBullet);
   }
 
   // Creates a new bullet
@@ -31,8 +31,39 @@ class BulletController {
 
   // Queries the database for all bullets
   private async getBullets(req : Request, resp : Response) {
-    let res = await db.query("SELECT * FROM BULLETS");
-    resp.send(res);
+    try {
+      let res = await db.query("SELECT * FROM BULLETS");
+      resp.sendStatus(200);
+      return res;
+    } catch (e) {
+      resp.send(e);
+    }
+  }
+
+  // Update bullet
+  private async updateBullet(req : Request, resp : Response) {
+    try {
+      let bulletId = req.params.id
+      let updatedBullet = new Bullet().createBullet(req.body);
+      await db.query(`UPDATE BULLETS SET TITLE='${updatedBullet.title}', DETAILS='${updatedBullet.details}',DATE='${updatedBullet.date}', STATUS='${updatedBullet.status}' WHERE ID=${bulletId}`);
+      resp.sendStatus(200);
+      return updatedBullet;
+    } catch (e) {
+      resp.send(e);
+    }
+  }
+
+  // Delete bullet
+  private async deleteBullet(req : Request, resp : Response) {
+    try {
+      let bulletId = req.params.id;
+      console.log(bulletId);
+      await db.query(`DELETE FROM BULLETS WHERE ID=${bulletId}`);
+      resp.sendStatus(200);
+      return bulletId;
+    } catch (e) {
+      resp.send(e);
+    }
   }
 }
 
